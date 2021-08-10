fb3dd1b5f5f36c4da05fb14d51d661bdcf226c45
nikos <nikos.evo@hotmail.com>
2021-08-10 15:24:17 +0200

    basic admin changes

---
+++ left-sidebar.php
@@ -0,0 +1,67 @@
+<aside id="sidebar-left" class="sidebar-left">
+				
+                <div class="sidebar-header">
+                    <div class="sidebar-title">
+                        Navigation
+                    </div>
+                    <div class="sidebar-toggle hidden-xs" data-toggle-class="sidebar-left-collapsed" data-target="html" data-fire-event="sidebar-left-toggle">
+                        <i class="fa fa-bars" aria-label="Toggle sidebar"></i>
+                    </div>
+                </div>
+            
+                <div class="nano">
+                    <div class="nano-content">
+                        <nav id="menu" class="nav-main" role="navigation">
+                            <ul class="nav nav-main">
+                                <li class="nav-active">
+                                    <a href="index.php">
+                                        <i class="fa fa-home" aria-hidden="true"></i>
+                                        <span>Dashboard</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="mailbox-folder.php">
+                                        <span class="pull-right label label-primary">182</span>
+                                        <i class="fa fa-envelope" aria-hidden="true"></i>
+                                        <span>Mailbox</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="subjects-table.php">
+                                        <span class="pull-right label label-primary">90</span>
+                                        <i class="fa fa-book" aria-hidden="true"></i>
+                                        <span>subjects</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="students-table.php">
+                                        <span class="pull-right label label-primary">340</span>
+                                        <i class="fa fa-user" aria-hidden="true"></i>
+                                        <span>Students</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="professors-table.php">
+                                        <span class="pull-right label label-primary">100</span>
+                                        <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                                        <span>Professors</span>
+                                    </a>
+                                </li>
+                               
+                                
+
+
+
+
+
+
+                            </ul>
+                        </nav>
+            
+
+
+                    </div>
+            
+                </div>
+            
+            </aside>
