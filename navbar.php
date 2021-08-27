5da1379153554b9368f50ab0c05dc6a71db9ba44
nikos <nikos.evo@hotmail.com>
2021-08-27 16:09:28 +0200

    teacher dashboard

--- navbar.php
+++ navbar.php
@@ -20,7 +20,7 @@
 		$sql=mysqli_query($link,"SELECT * FROM users where userID='$userID' ");
 		$row  = mysqli_fetch_array($sql);
 		$firstname = $row['fname']; 
-		$lastname  = $row['lname']; 
+		$la\stname  = $row['lname']; 
 		$role="student" ;
 	}
 	elseif($roleID==3){
