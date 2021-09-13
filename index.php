607a98805f11ea3632b55e86e27756c7ab3f3077
Nikos Tzekas <nikos.evo@hotmail.com>
2021-09-13 13:53:30 +0200

    notification

--- index.php
+++ index.php
@@ -42,29 +42,6 @@
 
 		<!-- Head Libs -->
 		<script src="assets/vendor/modernizr/modernizr.js"></script>
-
-		<style>
-		.alert {
-		padding: 20px;
-		background-color: #f44336;
-		color: white;
-		}
-
-		.closebtn {
-		margin-left: 15px;
-		color: white;
-		font-weight: bold;
-		float: right;
-		font-size: 22px;
-		line-height: 20px;
-		cursor: pointer;
-		transition: 0.3s;
-		}
-
-		.closebtn:hover {
-		color: black;
-		}
-		</style>
 	</head>		
 		
 	
