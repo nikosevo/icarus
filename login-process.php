ee5da2dd14fb3578e754d16c8a38df4a9092ae0f
Nikos Tzekas <nikos.evo@hotmail.com>
2021-09-13 13:16:48 +0200

    not letting the user sing in

--- login-process.php
+++ login-process.php
@@ -7,6 +7,13 @@ if(isset($_POST['save'])&& $_POST['save']== "signin")
     $password = md5($pwd);
     $sql=mysqli_query($link,"SELECT * FROM users where username='$username' and passwd='$password'");
     $row  = mysqli_fetch_array($sql);
+    if($row['isactive'] == 0 ){
+        echo "Waiting for adming to aprove";
+        //break here
+        header("Location: index.php"); 
+        exit();
+
+    }
     if(is_array($row))
     {
         $_SESSION["userID"] = $row['userID'];
