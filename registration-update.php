5df7bf592db80d1d8e3a95efaf59d7c7914d7be5
GiorgosKordis <icsd18098@icsd.aegean.gr>
2021-09-17 19:01:12 +0300

    register

--- registration-update.php
+++ registration-update.php
@@ -2,20 +2,34 @@
 	include 'connect.php';
     session_start();
 
-    $stid= $_SESSION["userID"];
+
+
+    $stid= $_GET['stid'];
     
     extract($_POST);
 
 	if(isset($_POST['save']) && $_POST['save']== "save"){
         
-		mysqli_autocommit($link, false);
-		foreach ($_POST['subject'] as $subject){
-            $sql1=mysqli_query($link,"SELECT tuiID FROM tuition where subID=$subject");
+		
+		foreach ($_POST['subjects'] as $subjects){
+
+            
+            
+            $sql1=mysqli_query($link,"SELECT * FROM tuition where subID=$subjects");
             $row1=mysqli_fetch_array($sql1);
+           
+            mysqli_autocommit($link, false);
             $tuID=$row1['tuiID'];
+             
             $sql3=mysqli_query($link,"INSERT INTO isregistered (Tgrade,Lgrade,Fgrade,tuiID,stdID) VALUES (0,0,0,$tuID,$stid)");
             
-            mysqli_commit($link);
+            echo $tuID;
+            
+				if($sql3){
+                    echo $stid;
+                    mysqli_commit($link);
+				}
+            
             
             
         } 
@@ -23,8 +37,8 @@
         
         
        
-        header("Location: profile.php?stdID=$stid");
-        exit();
+        // header("Location: profile.php?stdID=3");
+        // exit();
 		
 	} 
     elseif(isset($_POST['save']) && $_POST['save']== "final"){
